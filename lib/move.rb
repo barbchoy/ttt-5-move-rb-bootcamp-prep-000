5cce61a13aa9a49df6cb1ea50768b0e728e05871
Barbara Choy <barbchoy@gmail.com>
2018-01-04 02:04:31 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -2,7 +2,7 @@ def input_to_index(str)
   str.to_i - 1
 end
 
-def move(board_arr, index, char="X")
+def move(board_arr, index, char = "X")
   update_array_at_with(board_arr. index, char)
   true
 end
