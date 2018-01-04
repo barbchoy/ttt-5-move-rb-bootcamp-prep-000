6541e33ff9081e681fb65f175434f361b417bc91
Barbara Choy <barbchoy@gmail.com>
2018-01-04 02:04:10 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -3,5 +3,10 @@ def input_to_index(str)
 end
 
 def move(board_arr, index, char="X")
+  update_array_at_with(board_arr. index, char)
   true
+end
+
+def update_array_at_with(array, index, value)
+  array[index] = value
 end
